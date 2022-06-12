88c9749a3cb75ce746e1c1a5bc9cdac5b106b312
lumix5 <blod228@gmail.com>
2022-06-13 04:18:21 +0700

    pricec

--- pages/api/operatorVote.ts
+++ pages/api/operatorVote.ts
@@ -6,24 +6,25 @@ import { Prisma } from '@prisma/client'
 export default async function (req, res) {
   const { rating, userId, operatorId } = req.query;
 
-
-  try {
-
-    let vote = await prisma.operatorVotes.upsert({
-      where: { operatorId_userId: { operatorId, userId } },
-      update: {
-        rating: rating <= 5 ? new Prisma.Decimal(rating) : undefined,
-      },
-      create: {
-        operatorId: operatorId,
-        rating: new Prisma.Decimal(rating),
-        // @ts-ignore
-        userId: rating <= 5 ? new Prisma.Decimal(rating) : undefined,
-      },
-    });
-    res.status(200).json(vote);
-  } catch (err) {
-    console.log(err);
-    res.status(403).json({ err: "Error occured." });
+  if (rating <= 5) {
+    try {
+      let vote = await prisma.operatorVotes.upsert({
+        where: { operatorId_userId: { operatorId, userId } },
+        update: {
+          rating: new Prisma.Decimal(rating)
+        },
+        create: {
+          operatorId: operatorId,
+          rating: new Prisma.Decimal(rating),
+          // @ts-ignore
+          userId: userId,
+        },
+      });
+      res.status(200).json(vote);
+    } catch (err) {
+      console.log(err);
+      res.status(403).json({ err: "Error occured." });
+    }
   }
+  res.status(403).json({ err: "not valid number." });
 }
