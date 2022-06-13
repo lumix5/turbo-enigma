4e5a79360c76d3fce1900b12697326c89f38406d
lumix5 <blod228@gmail.com>
2022-06-13 19:15:33 +0700

    loading value bug

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -1,15 +1,17 @@
 import { MouseEventHandler, useEffect, useState } from "react";
 import { useRouterRefresh } from "../pages/operators";
 import { Rating } from "@mui/material";
-import { useLocalStorage } from "@mantine/hooks";
+import {useLocalStorage, useMediaQuery} from "@mantine/hooks";
 import { prisma } from "../db";
 import axios from "axios";
 import {useTheme} from "next-themes";
 import StarIcon from '@mui/icons-material/Star';
+import {Loading} from "@nextui-org/react";
 
 const StarRating = ({ ratedEntity }: any) => {
   const { theme, setTheme } = useTheme()
-
+  const [isLoading, setIsLoading] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 900px)", false);
 
   let [value, setValue] = useLocalStorage({
     key: ratedEntity.id,
@@ -20,22 +22,27 @@ const StarRating = ({ ratedEntity }: any) => {
   let refresh = useRouterRefresh();
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center relative items-center">
       <Rating
         name="half-rating"
         value={value}
         precision={0.5}
         emptyIcon={<StarIcon style={{  color: theme === 'dark' ? 'white' : ''}} fontSize="inherit" />}
         onChange={async (event: any, newValue: number) => {
-          setValue(newValue);
+          setIsLoading(true);
+          setValue(newValue === null ? value : newValue)
+
           await axios.post(`/api/operatorVote`, null, { params: {
               operatorId: ratedEntity.id,
-              rating: newValue,
+              rating: newValue === null ? value : newValue,
               userId: userid,
             }})
           refresh();
+          setIsLoading(false);
         }}
       />
+    {isLoading && isDesktop && <Loading css={{right: 0, position: "absolute"}}/>}
+      {isLoading && !isDesktop && <Loading size="xs"/>}
     </div>
   );
 };
